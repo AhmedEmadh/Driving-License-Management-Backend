a73412a95f4293a775f15bb0890d0573331119cf
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2025-11-10 23:17:44 +0200

    Added LicenseClassDTO, LicenceClassesController and Implemented Get APIs

---
+++ Driving-License-Management-Backend/Controllers/LicenseClassesController.cs
@@ -0,0 +1,54 @@
+﻿using Driving_License_Management_Backend.DTOs;
+using Driving_License_Management_BusinessLogicLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Driving_License_Management_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LicenseClassesController : ControllerBase
+    {
+        [HttpGet, ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetAllLicenseClasses()
+        {
+            var licenseClasses = clsLicenseClass.GetAllLicenseClassesList();
+            List<LicenseClassDTO> licenseClassDTOList = new List<LicenseClassDTO>();
+            foreach (var licenseClass in licenseClasses)
+            {
+                var licenseClassDTO = new LicenseClassDTO(licenseClass);
+                licenseClassDTOList.Add(licenseClassDTO);
+            }
+            return Ok(licenseClassDTOList);
+        }
+        [HttpGet("{ID}"), ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetLicenseClassByID(int ID)
+        {
+            var licenseClass = clsLicenseClass.Find(ID);
+            if (licenseClass == null)
+            {
+                return NotFound("License Class is not found");
+            }
+            return Ok(new LicenseClassDTO(licenseClass));
+
+        }
+
+        [HttpPost, ProducesResponseType(StatusCodes.Status201Created), ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult AddNewLicenseClass()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{ID}"), ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult UpdateLicenseClass(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{ID}"), ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteLicenseClass(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Driving-License-Management-Backend/DTOs/LicenseClassDTO.cs
@@ -0,0 +1,36 @@
+﻿using Driving_License_Management_BusinessLogicLayer;
+
+namespace Driving_License_Management_Backend.DTOs
+{
+    public class LicenseClassDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte MinimumAllowedAge { get; set; }
+        public byte DefaultValidityLengthInYears { get; set; }
+        public float ClassFees { get; set; }
+
+        public LicenseClassDTO() { }
+        public LicenseClassDTO(clsLicenseClass licenseClass)
+        {
+            this.ID = licenseClass.LicenseClassID;
+            this.Name = licenseClass.ClassName;
+            this.Description = licenseClass.ClassDescription;
+            this.MinimumAllowedAge = licenseClass.MinimumAllowedAge;
+            this.DefaultValidityLengthInYears = licenseClass.DefaultValidityLength;
+            this.ClassFees = licenseClass.ClassFees;
+
+        }
+        public void MapToEntity(clsLicenseClass licenseClass)
+        {
+            licenseClass.LicenseClassID = this.ID;
+            licenseClass.ClassName = this.Name;
+            licenseClass.ClassDescription = this.Description;
+            licenseClass.MinimumAllowedAge = this.MinimumAllowedAge;
+            licenseClass.DefaultValidityLength = this.DefaultValidityLengthInYears;
+            licenseClass.ClassFees = this.ClassFees;
+
+        }
+    }
+}
